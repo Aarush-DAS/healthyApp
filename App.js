25ed634c913bd14cb531254ec1bda6a1a446a088
Aarush-DAS <aarushdas2007@gmail.com>
2023-01-11 18:04:12 +0530

    class
    Co-authored-by: parwati187 <parwati187@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {NavigationContainer } from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack"
+import LoginScreen from "./screen/LoginScreen";
+import Registration from "./screen/registration";
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
+
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+  
+}else {firebase.app}
+const stack=createStackNavigator();
+
+const StackNav = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen}/>
+      <Stack.Screen name="registration" component={registrationScreen}/>
+      <Stack.Screen name="Drawer" component={DrawerNavigator}/>
+    </Stack.Navigator>
+  )
+
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <DrawerNavigator/>
+    </NavigationContainer>)
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screen/DashBoard.js
+++ screen/DashBoard.js
@@ -26,4 +26,57 @@ export default class DashBoard extends Component {
         
     }
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#15193c",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    droidSafeArea: {
+      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appIcon: {
+      width: RFValue(200),
+      height: RFValue(200),
+      resizeMode: "contain",
+      marginBottom: RFValue(20)
+    },
+    appTitleText: {
+      color: "white",
+      textAlign: "center",
+      fontSize: RFValue(40),
+      fontFamily: "Bubblegum-Sans",
+      marginBottom: RFValue(20)
+    },
+    textinput: {
+      width: RFValue(250),
+      height: RFValue(50),
+      padding: RFValue(10),
+      borderColor: "#FFFFFF",
+      borderWidth: RFValue(4),
+      borderRadius: RFValue(10),
+      fontSize: RFValue(20),
+      color: "#FFFFFF",
+      backgroundColor: "#15193c",
+      fontFamily: "Bubblegum-Sans"
+    },
+    button: {
+      width: RFValue(250),
+      height: RFValue(50),
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      borderRadius: RFValue(30),
+      backgroundColor: "white",
+      marginBottom: RFValue(20)
+    },
+    buttonText: {
+      fontSize: RFValue(24),
+      color: "#15193c",
+      fontFamily: "Bubblegum-Sans"
+    }
+  });
+  
+  
 
